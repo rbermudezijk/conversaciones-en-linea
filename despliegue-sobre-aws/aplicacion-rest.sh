2b91e8da9d9d2cc0f279867d7968bd65c96ec1ad
rbermudezijk <ricardob.sistemas@gmail.com>
2019-08-06 11:04:21 -0500

    despliegue sobre aws

--- despliegue-sobre-aws/aplicacion-rest.sh
+++ despliegue-sobre-aws/aplicacion-rest.sh
@@ -12,13 +12,36 @@ yum -y install git
 yum -y install httpd
 
 #PHP System
-yum -y install php php-mysql
-
-#Start HTTP - PHP server
-systemctl start httpd.service
+#yum -y install php php-mysql
+#sudo yum -y erase php php-pdo php-mysql php-common
+#source: https://www.linuxtechi.com/install-php-7-centos-7-rhel-7-server/
+yum -y install epel-release yum-utils
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager --enable remi-php71
+yum -y install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
 
 #Chat system
 git clone https://github.com/rbermudezijk/conversaciones-en-linea.git
 cp -a ./conversaciones-en-linea/aplicacion-rest/. /var/www/html
 rm -rf conversaciones-en-linea
 rm -f /var/www/html/Dockerfile
+
+#Give permissions to web system folder.
+#Source: https://www.centos.org/forums/viewtopic.php?t=54246
+echo -e '
+<Directory "/var/www/html">
+    Options Indexes FollowSymLinks ExecCGI
+    AllowOverride All
+    Allow from all
+    Require all granted
+</Directory>
+' > conf.d/api.conf
+
+chmod -R 755 /var/www/html
+restorecon -R /var/www
+
+# Use to enable external connections.
+sudo setsebool -P httpd_can_network_connect 1
+
+#Start HTTP - PHP server
+systemctl start httpd.service
